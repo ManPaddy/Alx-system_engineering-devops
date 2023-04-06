cc7ca11c8b765d7568cf27e1ef0cb849c23fb865
ManPaddy <https://github.com/ManPaddy/alx-system_engineering-devops.git>
2023-04-06 08:43:37 -0700

    Configurations Tasks

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
 #Create a file in /tmp
 #The file have permission of '0744'
 #File owner and group is 'www-data'
-{	File		=> '/tmp/school':
+
+	File {'/tmp/school':
 	Permission 	=> '0744',
 	Owner 		=> 'www-data',
 	Group		=> 'www-data',
-	Contents 	=> 'I love Puppet'
+	Contents 	=> 'I love Puppet',
 }
