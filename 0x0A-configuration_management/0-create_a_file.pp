4386b0de44fda1673bfb7445ad628051fa58a72a
ManPaddy <https://github.com/ManPaddy/alx-system_engineering-devops.git>
2023-04-06 16:28:24 -0700

    fixing code

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Create a file in /tmp.
 # The file have permission of 0744
-# File owner and group is iwww-data
+# File owner and group is www-data
 
-'file' { '/tmp/school':
+file{ '/tmp/school':
 mode	=> '0744',
 owner 	=> 'www-data',
 group	=> 'www-data',
