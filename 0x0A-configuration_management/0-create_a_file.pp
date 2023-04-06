67587bbaab21700f5ea9b5a20bc0a1277abdc212
ManPaddy <https://github.com/ManPaddy/alx-system_engineering-devops.git>
2023-04-06 09:00:20 -0700

    fixing 1

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,8 +3,8 @@
 #File owner and group is 'www-data'
 
 file { '/tmp/school':
-mode	=> '0744',
-owner 	=> 'www-data',
-group	=> 'www-data',
-content => 'I love Puppet',
+mode	=>'0744',
+owner 	=>'www-data',
+group	=>'www-data',
+content =>'I love Puppet',
 }
